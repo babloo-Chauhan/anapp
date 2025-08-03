69be5e7993951f004ce9de4c15d7973f9e7ba8d2
Babloo Chauhan <b8416800849@gmail.com>
2025-08-04 02:44:34 +0530

    #

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,16 +1,16 @@
-﻿import React, { } from 'react';
+﻿import React, {useState } from 'react';
 import { BiSolidUserCircle } from 'react-icons/bi';
 import { TiThMenu } from 'react-icons/ti';
 import { FaXmark } from 'react-icons/fa6';
 
 const Navbar = React.memo(function Navbar({  toggleLayout , i }) {
-    // const [isOpen, setIsOpen] = useState(false);
+    const [isOpen, setIsOpen] = useState(i);
 
-    // const handleToggleLayout = () => {
-    //     setIsOpen(!isOpen);
-    //     onLeftClick(); // Call the function passed from Card component to toggle sidebar
+    const handleToggleLayout = () => {
+        // setIsOpen(!isOpen);
+        toggleLayout() // Call the function passed from Card component to toggle sidebar
       
-    // };
+     };
     // console.log(toggleLayout(true));
   
 
@@ -20,13 +20,13 @@ const Navbar = React.memo(function Navbar({  toggleLayout , i }) {
 
             <div
                 className='h-[50px] w-[50px] rounded-full bg-white flex justify-center items-center cursor-pointer'
-                onClick={toggleLayout}
+                onClick={handleToggleLayout}
             >
                 {i ? (
                     <TiThMenu className='text-4xl transition-transform duration-300 transform rotate-0' />
                 ) : (
                    
-                <FaXmark className='text-3xl transition-transform duration-300 transform rotate-180' />
+                        <FaXmark className='text-3xl transition-transform duration-300 transform rotate-180' />
                 )}
             </div>
 
